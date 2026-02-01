a52058fd9a8bbadaf7b1ac2ed93e6add7605f3c7
Radhika Irkal <radhikamirkal@gmail.com>
2026-02-01 13:28:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function removecolor() {
+  const select = document.getElementById("colorSelect");
+
+  // Remove the selected option
+  select.remove(select.selectedIndex);
+}
